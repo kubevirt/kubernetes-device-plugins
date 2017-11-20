b7ca11209743ee1ea9b458acf7a6c7a91d89ba15
Martin Polednik <mpolednik@redhat.com>
2017-11-20 16:15:26 +0100

    progress: spawn multiple gRPC servers, one for each device class
    multiple changes squashed together, namely
    
    * 1 gRPC server/device class
    * commited vendor dir
    * progress on VFIO front
    
    Signed-off-by: Martin Polednik <mpolednik@redhat.com>

--- cmd/server.go
+++ cmd/server.go
@@ -23,6 +23,17 @@
 package main
 
 import (
+	"flag"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"path"
+	"syscall"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/golang/glog"
 	"github.com/mpolednik/linux-vfio-k8s-dpi/pkg"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
@@ -30,21 +41,206 @@ import (
 )
 
 const (
-	resourceName = "mpolednik.github.io/linux-vfio-k8s-dpi"
-	serverSock   = pluginapi.DevicePluginPath + "linux-vfio-k8s-dpi.sock"
+	resourceNamespace = "mpolednik.github.io/"
 )
 
-type LinuxVfioDPI struct {
-	socket  string
-	devices []*pluginapi.Device
-	server  grpc.Server
+type DevicePlugin struct {
+	devs         []*pluginapi.Device
+	socket       string
+	stop         chan interface{}
+	server       *grpc.Server
+	resourceName string
 }
 
-func (dpi *LinuxVfioDPI) ListAndWatch(_ *pluginapi.Empty, server pluginapi.DevicePlugin_ListAndWatchServer) {
-	server.Send(&pluginapi.ListAndWatchResponse{Devices: pci.Discover()})
+func NewDevicePlugin(deviceID string, deviceIDs []string) *DevicePlugin {
+	var devs []*pluginapi.Device
+
+	for _, deviceID := range deviceIDs {
+		devs = append(devs, &pluginapi.Device{
+			ID:     deviceID,
+			Health: pluginapi.Healthy,
+		})
+	}
+
+	glog.V(3).Infof("Creating device plugin %s, initial devices %v", deviceID, devs)
+	return &DevicePlugin{
+		socket:       pluginapi.DevicePluginPath + deviceID,
+		devs:         devs,
+		resourceName: resourceNamespace + deviceID,
+		stop:         make(chan interface{}),
+	}
 }
 
-func (dpi *LinuxVfioDPI) Allocate(ctx context.Context, r *pluginapi.AllocateRequest) (*pluginapi.AllocateResponse, error) {
-	response := &pluginapi.AllocateResponse{}
-	return response, nil
+// Start starts the gRPC server of the device plugin
+func (dpi *DevicePlugin) start() error {
+	glog.V(3).Info("Starting the DPI gRPC server")
+
+	err := dpi.cleanup()
+	if err != nil {
+		glog.Errorf("Failed to setup a DPI gRPC server: %s", err)
+		return err
+	}
+
+	sock, err := net.Listen("unix", dpi.socket)
+	if err != nil {
+		glog.Errorf("Failed to setup a DPI gRPC server: %s", err)
+		return err
+	}
+
+	dpi.server = grpc.NewServer([]grpc.ServerOption{}...)
+	pluginapi.RegisterDevicePluginServer(dpi.server, dpi)
+
+	go dpi.server.Serve(sock)
+	glog.V(3).Info("Serving requests...")
+	// Wait till grpc server is ready.
+	for i := 0; i < 10; i++ {
+		services := dpi.server.GetServiceInfo()
+		if len(services) > 1 {
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	return nil
+}
+
+// Stop stops the gRPC server
+func (dpi *DevicePlugin) Stop() error {
+	glog.V(3).Info("Stopping the DPI gRPC server")
+	dpi.server.Stop()
+	close(dpi.stop)
+
+	return dpi.cleanup()
+}
+
+// Register registers the device plugin for the given resourceName with Kubelet.
+func (dpi *DevicePlugin) register(kubeletEndpoint, resourceName string) error {
+	glog.V(3).Info("Registering the DPI with Kubelet")
+
+	conn, err := grpc.Dial(kubeletEndpoint, grpc.WithInsecure(),
+		grpc.WithDialer(func(addr string, timeout time.Duration) (net.Conn, error) {
+			return net.DialTimeout("unix", addr, timeout)
+		}))
+	defer conn.Close()
+	if err != nil {
+		glog.Errorf("Could not dial gRPC: %s", err)
+		return err
+	}
+	client := pluginapi.NewRegistrationClient(conn)
+	glog.Infof("Registration for endpoint %s resourceName %s", path.Base(dpi.socket), resourceName)
+	reqt := &pluginapi.RegisterRequest{
+		Version:      pluginapi.Version,
+		Endpoint:     path.Base(dpi.socket),
+		ResourceName: resourceName,
+	}
+
+	_, err = client.Register(context.Background(), reqt)
+	if err != nil {
+		glog.Errorf("Registration failed: %s", err)
+		return err
+	}
+	return nil
+}
+
+// ListAndWatch lists devices and update that list according to the Update call
+func (dpi *DevicePlugin) ListAndWatch(e *pluginapi.Empty, s pluginapi.DevicePlugin_ListAndWatchServer) error {
+	var devs []*pluginapi.Device
+
+	for _, d := range dpi.devs {
+		devs = append(devs, &pluginapi.Device{
+			ID:     d.ID,
+			Health: pluginapi.Healthy,
+		})
+	}
+
+	s.Send(&pluginapi.ListAndWatchResponse{Devices: devs})
+
+	for {
+		select {
+		case <-dpi.stop:
+			return nil
+		}
+	}
+}
+
+func (dpi *DevicePlugin) Allocate(ctx context.Context, r *pluginapi.AllocateRequest) (*pluginapi.AllocateResponse, error) {
+	var response pluginapi.AllocateResponse
+
+	for _, id := range r.DevicesIDs {
+		iommuGroup, err := pci.GetIOMMUGroup(id)
+		if err != nil {
+			return &response, err
+		}
+		vfioPath := pci.ConstructVFIOPath(iommuGroup)
+		glog.V(3).Infof("Got request to allocate device %s from IOMMU group %d (path %s)", id, iommuGroup, vfioPath)
+		dev := new(pluginapi.DeviceSpec)
+		response.Devices = append(response.Devices, dev)
+	}
+
+	return &response, nil
+}
+
+// Serve starts the gRPC server and register the device plugin to Kubelet
+func (dpi *DevicePlugin) Serve() error {
+	err := dpi.start()
+	if err != nil {
+		return err
+	}
+
+	err = dpi.register(pluginapi.KubeletSocket, dpi.resourceName)
+	if err != nil {
+		dpi.Stop()
+		return err
+	}
+
+	return nil
+}
+
+func (dpi *DevicePlugin) cleanup() error {
+	if err := os.Remove(dpi.socket); err != nil && !os.IsNotExist(err) {
+		glog.Errorf("Could not clean up socket %s: %s", dpi.socket, err)
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	var plugins []*DevicePlugin
+	devices := pci.Discover()
+
+	for deviceClass, deviceIDs := range *devices {
+		plugins = append(plugins, NewDevicePlugin(deviceClass, deviceIDs))
+	}
+
+	for _, plugin := range plugins {
+		go plugin.Serve()
+	}
+
+	watcher, _ := fsnotify.NewWatcher()
+	defer watcher.Close()
+	watcher.Add(pluginapi.DevicePluginPath)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+L:
+	for {
+		select {
+		case err := <-watcher.Errors:
+			log.Printf("inotify: %s", err)
+		case s := <-sigs:
+			switch s {
+			default:
+				log.Printf("Received signal \"%v\", shutting down", s)
+
+				for _, plugin := range plugins {
+					plugin.Stop()
+				}
+				break L
+			}
+		}
+	}
 }
--- pkg/pci.go
+++ pkg/pci.go
@@ -4,14 +4,16 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/golang/glog"
-	pluginapi "k8s.io/kubernetes/pkg/kubelet/apis/deviceplugin/v1alpha"
 )
 
-func Discover() []*pluginapi.Device {
-	var devices []*pluginapi.Device
+type DeviceMap map[string][]string
+
+func Discover() *DeviceMap {
+	var devices = make(DeviceMap)
 
 	filepath.Walk("/sys/bus/pci/devices", func(path string, info os.FileInfo, err error) error {
 		glog.V(3).Infof("Discovering device in %s", path)
@@ -26,16 +28,36 @@ func Discover() []*pluginapi.Device {
 			return filepath.SkipDir
 		}
 
-		devices = append(devices, &pluginapi.Device{
-			ID:     formatDeviceID(vendorID, deviceID),
-			Health: pluginapi.Healthy,
-		})
+		deviceClass := formatDeviceID(vendorID, deviceID)
+		devices[deviceClass] = append(devices[deviceClass], info.Name())
 
 		return nil
 	})
 
 	glog.V(3).Infof("Discovered devices: %s", devices)
-	return devices
+	return &devices
+}
+
+func GetIOMMUGroup(deviceAddress string) (int, error) {
+	iommuPath, err := os.Readlink(filepath.Join("/sys/bus/pci/devices", deviceAddress, "iommu_group"))
+	if err != nil {
+		glog.Errorf("Could not read IOMMU group for device %s: %s", deviceAddress, err)
+		return -1, err
+	}
+
+	_, file := filepath.Split(iommuPath)
+
+	iommuGroup, err := strconv.ParseInt(file, 10, 32)
+	if err != nil {
+		glog.Errorf("Could not parse IOMMU group for device %s: %s", deviceAddress, err)
+		return -1, err
+	}
+
+	return int(iommuGroup), nil
+}
+
+func ConstructVFIOPath(iommuGroup int) string {
+	return filepath.Join("dev", "vfio", strconv.FormatInt(int64(iommuGroup), 10))
 }
 
 func getDeviceVendor(deviceAddress string) (string, string, error) {
@@ -57,5 +79,5 @@ func getDeviceVendor(deviceAddress string) (string, string, error) {
 }
 
 func formatDeviceID(vendorID string, deviceID string) string {
-	return strings.Join([]string{vendorID, deviceID}, ":")
+	return strings.Join([]string{vendorID, deviceID}, "_")
 }
