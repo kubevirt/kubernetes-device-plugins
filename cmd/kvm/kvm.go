7697dca679f4cfd5f0596c8de18ef4d5139b4d0f
Petr Horáček <phoracek@redhat.com>
2018-03-06 21:43:11 +0100

    dpm was moved to a separate repo, fix dependencies and imports
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- cmd/kvm/kvm.go
+++ cmd/kvm/kvm.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 
-	"kubevirt.io/kubernetes-device-plugins/pkg/dpm"
+	"github.com/kubevirt/device-plugin-manager/pkg/dpm"
 	"kubevirt.io/kubernetes-device-plugins/pkg/kvm"
 )
 
--- cmd/network/bridge/bridge.go
+++ cmd/network/bridge/bridge.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	"github.com/golang/glog"
-	"kubevirt.io/kubernetes-device-plugins/pkg/dpm"
+	"github.com/kubevirt/device-plugin-manager/pkg/dpm"
 	"kubevirt.io/kubernetes-device-plugins/pkg/network/bridge"
 )
 
